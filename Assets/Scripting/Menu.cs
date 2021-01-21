76b0516e1bbd8838cf68e5a1947ec3759d40450f
Saalness <55344407+Saalness@users.noreply.github.com>
2021-01-21 12:48:11 +0400

    Revert "menu and stuff"
    This reverts commit 571a69bd9b5950e4cfff96d57892e0f03b94cc62.

--- Assets/Scripting/Menu.cs
+++ Assets/Scripting/Menu.cs
@@ -6,9 +6,7 @@ using UnityEngine.SceneManagement;
 public class Menu : MonoBehaviour
 {
     public GameObject Main_menu;
-    public GameObject Tutorial;
-    public GameObject Settings;
-    public GameObject Controls;
+    public GameObject Food_selection;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,38 +24,14 @@ public class Menu : MonoBehaviour
         Debug.Log("The play button was clicked");
     }
     
-    public void Btn_tutorial_clicked()
+    public void Btn_Foodselect_menu_clicked()
     {
-        Tutorial.SetActive(true);
+        Food_selection.SetActive(true);
         Main_menu.SetActive(false);
-    }
-
-    public void Btn_back_to_menu()
-    {
-        Tutorial.SetActive(false);
-        Settings.SetActive(false);
-        Controls.SetActive(false);
-        Main_menu.SetActive(true);
     }
 
     public void Btn_quit_clicked()
     {
         Application.Quit();
-    }
-
-    public void Btn_settings_clicked()
-    {
-        Tutorial.SetActive(false);
-        Settings.SetActive(true);
-        Controls.SetActive(false);
-        Main_menu.SetActive(false);
-    }
-
-    public void Btn_controls_clicked()
-    {
-        
-        Settings.SetActive(false);
-        Controls.SetActive(true);
-        Main_menu.SetActive(false);
     }
 }
